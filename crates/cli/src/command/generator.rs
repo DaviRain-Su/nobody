ec7d5f95cf555ea3f2ef615b1680bb6034b4b23a
Davirain <davirain.yin@gmail.com>
2024-01-27 21:40:34 +0800

    Update generator.rs

--- crates/cli/src/command/generator.rs
+++ crates/cli/src/command/generator.rs
@@ -1,8 +1,7 @@
-use crate::config::Config;
-use crate::constant::DEFAULT_CONFIG_FILE;
 use crate::errors::Error;
-use solana_sdk::signer::keypair::{write_keypair, Keypair};
-use solana_sdk::signer::{EncodableKey, Signer};
+use serde::{Deserialize, Serialize};
+use solana_sdk::signer::keypair::Keypair;
+use solana_sdk::signer::Signer;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -32,6 +31,13 @@ impl Generator {
                 .collect::<Vec<_>>()
         );
 
+        let keypairs_str = KeyPairsString::from(keypairs);
+
+        let home_path = dirs::home_dir().ok_or(Error::Custom("can't open home dir".into()))?;
+        let nobody_config_path = home_path.join(".config").join("nobody");
+        let keypairs_path = nobody_config_path.join("keypairs.json");
+        keypairs_str.write(keypairs_path.clone())?;
+
         Ok(())
     }
 }
@@ -39,6 +45,64 @@ impl Generator {
 #[derive(Debug)]
 pub struct KeyPairs {
     pub keypairs: Vec<Keypair>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyPairsString {
+    pub keypairs: Vec<Item>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Item {
+    pub pubkey: String,
+    pub secret: String,
+}
+
+impl KeyPairsString {
+    pub fn write(&self, path: PathBuf) -> anyhow::Result<()> {
+        let temp_keypairs_str = serde_json::to_string(&self)?;
+        std::fs::write(path, temp_keypairs_str).map_err(|e| {
+            Error::from(format!(
+                "failed write keypairs_path: Error({})",
+                e.to_string()
+            ))
+        })?;
+        Ok(())
+    }
+
+    pub fn read(path: PathBuf) -> anyhow::Result<Self> {
+        let temp_keypairs_str = std::fs::read_to_string(path).map_err(|e| {
+            Error::from(format!(
+                "failed read keypairs_path: Error({})",
+                e.to_string()
+            ))
+        })?;
+        let keypairs_str = serde_json::from_str(&temp_keypairs_str).map_err(|e| {
+            Error::from(format!(
+                "failed deserialze keypairs_path: Error({})",
+                e.to_string()
+            ))
+        })?;
+        Ok(keypairs_str)
+    }
+}
+
+impl From<KeyPairs> for KeyPairsString {
+    fn from(keypairs: KeyPairs) -> Self {
+        let keypairs = keypairs
+            .keypairs
+            .iter()
+            .map(|k| {
+                let raw_keypairs = k.to_bytes();
+                Item {
+                    pubkey: k.pubkey().to_string(),
+                    secret: serde_json::to_string(&raw_keypairs.to_vec())
+                        .expect("serde keypairs error"),
+                }
+            })
+            .collect::<Vec<_>>();
+        Self { keypairs }
+    }
 }
 
 impl KeyPairs {
